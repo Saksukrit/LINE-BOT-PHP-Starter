0b1f25828522e6d0afcfbd2aee8c30cb1b8a9605
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-01-31 00:26:17 +0700

    .

--- bot.php
+++ bot.php
@@ -468,9 +468,6 @@ if (!is_null($events['events'])) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             $result = curl_exec($ch);
-            curl_close($ch);
-
-            echo $result . "\r\n";
 
             if($command == "continue"){
               $messages = [
@@ -478,30 +475,33 @@ if (!is_null($events['events'])) {
               'text' => 'ถามต่อเลย'
               ];
 
+              $url = 'https://api.line.me/v2/bot/message/reply';
+              $data = [
+              'replyToken' => $replyToken,
+              'messages' => [$messages],
+              ];
+              $post = json_encode($data);
+              $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+              $ch = curl_init($url);
+              curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+              curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+              curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+              curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+              $result = curl_exec($ch);
+
               $command = "non";
             }
 
-            $url = 'POST https://api.line.me/v2/bot/message/push';
-            $data = [
-            'replyToken' => $replyToken,
-            'messages' => [$messages],
-            ];
-            $post = json_encode($data);
-            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            $result = curl_exec($ch);
             curl_close($ch);
 
             echo $result . "\r\n";
 
-            //   $command = "non";
-            // }
+            
+
+            
 
 
 
