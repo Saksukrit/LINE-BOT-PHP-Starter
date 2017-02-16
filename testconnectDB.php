250a467ad35bc297dab951d1b75d822004b27e83
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-02-16 20:44:57 +0700

    .

--- testconnectDB.php
+++ testconnectDB.php
@@ -1,11 +1,16 @@
 
 <?php
-$link = mysql_connect('sql6.freemysqlhosting.net', 'sql6159246', 'DBUUiG4F5U');
+$link = mysql_connect('sql6.freemysqlhosting.net', 'sql6159246', 'DBUUiG4F5');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
 echo 'Connected successfully';
 mysql_close($link);
+
+
+echo 'okok';
+
+//  https://warm-brushlands-72856.herokuapp.com/testconnectDB.php
 ?>
 
 
@@ -14,3 +19,4 @@ Name: sql6159246
 Username: sql6159246
 Password: DBUUiG4F5U
 Port number: 3306 -->
+
