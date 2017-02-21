4e9e7a41379a073817446c350daa0df80e091bf5
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-02-21 18:32:56 +0700

    .

--- echo_bot.php
+++ echo_bot.php
@@ -1,53 +1,52 @@
 <?php
-
 /**
-* Copyright 2016 LINE Corporation
-*
-* LINE Corporation licenses this file to you under the Apache License,
-* version 2.0 (the "License"); you may not use this file except in compliance
-* with the License. You may obtain a copy of the License at:
-*
-*   https://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations
-* under the License.
-*/
-
-require_once('./LINEBotTiny.php');
-
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+require_once ('./LINEBotTiny.php');
 $channelAccessToken = 'uEaFS7lHeCcF0FEBVNQtuBTVpwVzjMCSebgBPdA/XUqgxzpYg8MHySfkmKpKys/TTEvQO99XihXnZaPKVO/4VsQXLqs8LQZdmskXuwncFHyI8/GZjv91J9Q/YN/pmATJTvlp6YOxOBypA2QFg1r6OwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '98ca0db8ed81032c7d483cef30bcb190';
-
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 echo $client->parseEvents();
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(
+
+foreach ($client->parseEvents() as $event)
+{
+    switch ($event['type'])
+    {
+    case 'message':
+        $message = $event['message'];
+        switch ($message['type'])
+        {
+        case 'text':
+            $client->replyMessage(array(
+                'replyToken' => $event['replyToken'],
+                'messages' => array(
                     array(
-                    'replyToken' => $event['replyToken'],
-                    'messages' => array(
-                    array('type' => 'text','text' => $message['text'])
+                        'type' => 'text',
+                        'text' => $message['text']
                     )
-                    )
-                    );
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
+                )
+            ));
+        break;
         default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
+            error_log("Unsupporeted message type: " . $message['type']);
+        break;
+        }
+    break;
+    default:
+        error_log("Unsupporeted event type: " . $event['type']);
+    break;
     }
 }
-
-
 echo "OK ?? ";
